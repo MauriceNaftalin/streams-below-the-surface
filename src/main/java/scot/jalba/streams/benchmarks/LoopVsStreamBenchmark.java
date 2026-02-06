c894e5d2517746a0b5d52024b5bd61173a6a6064
Cay Horstmann <cay@horstmann.com>
2026-02-06 19:03:44 +0100

    Benchmarks

--- src/main/java/scot/jalba/streams/benchmarks/LoopVsStreamBenchmark.java
+++ src/main/java/scot/jalba/streams/benchmarks/LoopVsStreamBenchmark.java
@@ -1,8 +1,6 @@
 package scot.jalba.streams.benchmarks;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
 import module java.base;
@@ -13,10 +11,26 @@ public class LoopVsStreamBenchmark {
 
     @State(Scope.Benchmark)
     public static class BenchmarkState {
-        public int LOOPS = 50_000;
-        public int POINTS = 20_000;
-        public int[] coords = randomIntArray(2 * POINTS);
+        @Param({
+                "200000|5000",
+                "50000|20000",
+                "20000|50000",
+                "5000|200000"
+        })
+        public String config;
+
+        @Setup(Level.Trial)
+        public void setup() {
+            String[] parts = config.split("\\|");
+            LOOPS = Integer.parseInt(parts[0]);
+            POINTS = Integer.parseInt(parts[1]);
+            coords = randomIntArray(2 * POINTS);
+        }
+        public int LOOPS;
+        public int POINTS;
+        public int[] coords;
     }
+
 
     @Benchmark
     public void streamInLoop(BenchmarkState state, Blackhole bh) {
@@ -50,6 +64,39 @@ public class LoopVsStreamBenchmark {
                     if (z < min) {
                         min = z;
                     }
+                }
+            }
+            bh.consume(min);
+        }
+    }
+
+    @Benchmark
+    public void streamInLoopNoLimit(BenchmarkState state, Blackhole bh) {
+        int[] coords = state.coords;
+        for (int k = 0; k < state.LOOPS; k++) {
+            long result = IntStream.range(0, state.POINTS).mapToLong(i -> {
+                        int x = coords[2 * i];
+                        int y = coords[2 * i + 1];
+                        return (long) x * x + (long) y * y;
+                    })
+                    .filter(p -> p > Integer.MAX_VALUE)
+                    .min()
+                    .orElseThrow();
+            bh.consume(result);
+        }
+    }
+
+    @Benchmark
+    public void loopInLoopNoLimit(BenchmarkState state, Blackhole bh) {
+        int[] coords = state.coords;
+        for (int k = 0; k < state.LOOPS; k++) {
+            long min = Long.MAX_VALUE;
+            for (int i = 0; i < state.POINTS; i++) {
+                int x = coords[2 * i];
+                int y = coords[2 * i + 1];
+                long z = (long) x * x + (long) y * y;
+                if (z < min) {
+                    min = z;
                 }
             }
             bh.consume(min);
